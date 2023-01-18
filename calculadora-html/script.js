a7d14b613161ae5f725a0a841f6c44d47bdf47a0
Jairo Vidomines <jvidomines@gmail.com>
2023-01-18 21:34:19 +0100

    review all projects

--- calculadora-html/script.js
+++ calculadora-html/script.js
@@ -3,85 +3,99 @@ let operandoB;
 let operacion;
 
 const init = () => {
-  let resultado = document.getElementById('resultado');
-  let reset = document.getElementById('reset');
-  let punto = document.getElementById('punto');
-  let masMenos = document.getElementById('masmenos');
-  let porcentaje = document.getElementById('porcentaje');
-  let division = document.getElementById('division');
-  let multiplicacion = document.getElementById('multiplicacion');
-  let resta = document.getElementById('resta');
-  let suma = document.getElementById('suma');
-  let cero = document.getElementById('cero');
-  let igual = document.getElementById('igual');
-  let uno = document.getElementById('uno');
-  let dos = document.getElementById('dos');
-  let tres = document.getElementById('tres');
-  let cuatro = document.getElementById('cuatro');
-  let cinco = document.getElementById('cinco');
-  let seis = document.getElementById('seis');
-  let siete = document.getElementById('siete');
-  let ocho = document.getElementById('ocho');
-  let nueve = document.getElementById('nueve');
+  const resultado = document.getElementById("resultado");
+  const reset = document.getElementById("reset");
+  const punto = document.getElementById("punto");
+  const masMenos = document.getElementById("masmenos");
+  const porcentaje = document.getElementById("porcentaje");
+  const division = document.getElementById("division");
+  const multiplicacion = document.getElementById("multiplicacion");
+  const resta = document.getElementById("resta");
+  const suma = document.getElementById("suma");
+  const cero = document.getElementById("cero");
+  const igual = document.getElementById("igual");
+  const uno = document.getElementById("uno");
+  const dos = document.getElementById("dos");
+  const tres = document.getElementById("tres");
+  const cuatro = document.getElementById("cuatro");
+  const cinco = document.getElementById("cinco");
+  const seis = document.getElementById("seis");
+  const siete = document.getElementById("siete");
+  const ocho = document.getElementById("ocho");
+  const nueve = document.getElementById("nueve");
 
   uno.onclick = function () {
     if (resultado.textContent.length <= 7) {
-      resultado.textContent = resultado.textContent + "1";
+      resultado.textContent += "1";
     }
   };
+
   dos.onclick = function () {
     if (resultado.textContent.length <= 7) {
-      resultado.textContent = resultado.textContent + "2";
+      resultado.textContent += "2";
     }
   };
+
   tres.onclick = function () {
     if (resultado.textContent.length <= 7) {
-      resultado.textContent = resultado.textContent + "3";
+      resultado.textContent += "3";
     }
   };
+
   cuatro.onclick = function () {
     if (resultado.textContent.length <= 7) {
-      resultado.textContent = resultado.textContent + "4";
+      resultado.textContent += "4";
     }
   };
+
   cinco.onclick = function () {
     if (resultado.textContent.length <= 7) {
-      resultado.textContent = resultado.textContent + "5";
+      resultado.textContent += "5";
     }
   };
+
   seis.onclick = function () {
     if (resultado.textContent.length <= 7) {
-      resultado.textContent = resultado.textContent + "6";
+      resultado.textContent += "6";
     }
   };
+
   siete.onclick = function () {
     if (resultado.textContent.length <= 7) {
-      resultado.textContent = resultado.textContent + "7";
+      resultado.textContent += "7";
     }
   };
+
   ocho.onclick = function () {
     if (resultado.textContent.length <= 7) {
-      resultado.textContent = resultado.textContent + "8";
+      resultado.textContent += "8";
     }
   };
+
   nueve.onclick = function () {
     if (resultado.textContent.length <= 7) {
-      resultado.textContent = resultado.textContent + "9";
+      resultado.textContent += "9";
     }
   };
+
   cero.onclick = function () {
     if (resultado.textContent.length <= 7) {
-      resultado.textContent = resultado.textContent + "0";
+      resultado.textContent += "0";
     }
   };
+
   punto.onclick = function () {
     if (resultado.textContent.indexOf(".") === -1) {
-      resultado.textContent = resultado.textContent + ".";
+      resultado.textContent += ".";
     }
   };
+
   masMenos.onclick = function () {
-    if (resultado.textContent === "" && resultado.textContent.indexOf("-") === -1) {
-      resultado.textContent = resultado.textContent + "-";
+    if (
+      resultado.textContent === "" &&
+      resultado.textContent.indexOf("-") === -1
+    ) {
+      resultado.textContent += "-";
     }
   };
 
@@ -96,6 +110,7 @@ const init = () => {
       limpiar();
     }
   };
+
   resta.onclick = function () {
     if (operacion !== "-") {
       operandoA = resultado.textContent;
@@ -103,13 +118,15 @@ const init = () => {
       limpiar();
     }
   };
+
   division.onclick = function () {
     if (operacion !== "/") {
       operandoA = resultado.textContent;
       operacion = "/";
-      limpiar(); 
+      limpiar();
     }
   };
+
   multiplicacion.onclick = function () {
     if (operacion !== "*") {
       operandoA = resultado.textContent;
@@ -117,18 +134,21 @@ const init = () => {
       limpiar();
     }
   };
+
   porcentaje.onclick = function () {
     if (operacion !== "%") {
       operandoA = resultado.textContent;
       operacion = "%";
-      limpiar(); 
+      limpiar();
     }
   };
+
   igual.onclick = function () {
     operandoB = resultado.textContent;
-    resolver(); 
+    resolver();
   };
 };
+
 init();
 
 const limpiar = () => {
@@ -144,7 +164,7 @@ const resetear = () => {
 
 const resolver = () => {
   let res = 0;
-  switch(operacion) {
+  switch (operacion) {
     case "+":
       res = parseFloat(operandoA) + parseFloat(operandoB);
       break;
@@ -153,30 +173,33 @@ const resolver = () => {
       break;
     case "*":
       res = parseFloat(operandoA) * parseFloat(operandoB);
-      break;     
+      break;
     case "/":
       if (operandoB == 0) {
         res = "Error";
       } else {
-      res = parseFloat(operandoA) / parseFloat(operandoB);
+        res = parseFloat(operandoA) / parseFloat(operandoB);
       }
+
       break;
     case "%":
       res = (parseFloat(operandoA) * parseFloat(operandoB)) / 100;
       break;
   }
+
   if (res.toString().length > 8) {
     resetear();
     resultado.textContent = res.toExponential(2);
   } else {
     resetear();
     resultado.textContent = res;
-  };
+  }
 };
 
 const check = () => {
   if (operandoA.length > 3) {
-    resultado.textcontent = operandoA("display error")
+    resultado.textcontent = operandoA("display error");
   }
 };
+
 check();
--- calculator-pro/calculator-pro.js
+++ calculator-pro/calculator-pro.js
@@ -2,71 +2,94 @@ let resultados = [];
 let promptNumbers = [];
 
 function calculadoraPro() {
+  // Introducir números
+  const getNumbers = () => {
+    const givenNumbers = [];
+    let number;
+    do {
+      number = Number(prompt("Enter a number"));
+      if (isNaN(number)) {
+        alert("Note: you must enter only numbers");
+        return getNumbers();
+      }
 
-//introducir números
-    const getNumbers = () => {
-        let givenNumbers = [];
-        let number;
-        do {
-            number = Number(prompt('Enter a number'));
-            if (isNaN(number)) {
-                alert('Note: you must enter only numbers');
-                return getNumbers();
-            }
-            if (givenNumbers.length < 1 && number === 0) {
-                alert('Note: you must enter a number');
-                return getNumbers();
-            }
-            if (number !== 0) {
-                givenNumbers.push(number);
-            }
-        }
-        while (number !== 0)
-        return givenNumbers
+      if (givenNumbers.length < 1 && number === 0) {
+        alert("Note: you must enter a number");
+        return getNumbers();
+      }
+
+      if (number !== 0) {
+        givenNumbers.push(number);
+      }
+    } while (number !== 0);
+
+    return givenNumbers;
+  };
+
+  promptNumbers = getNumbers();
+
+  // Calcular
+  const calcular = () => {
+    const calculos = [];
+    if (promptNumbers.length > 1) {
+      const suma = promptNumbers.reduce((sum, num) => sum + num);
+      const resta = promptNumbers.reduce((rest, num) => rest - num);
+      const multiplica = promptNumbers.reduce((mult, num) => mult * num);
+      const divide = promptNumbers.reduce((div, num) => div / num).toFixed(3);
+
+      calculos.push(
+        "\nAdd: " +
+          suma +
+          "\nSubtract: " +
+          resta +
+          "\nMultiply: " +
+          multiplica +
+          "\nDivide: " +
+          divide
+      );
+      return calculos;
     }
-    promptNumbers = getNumbers();
 
-    //calcular
-    const calcular = () => {
-        let calculos = [];
-        if (promptNumbers.length > 1) {
-            const suma = promptNumbers.reduce((sum, num) => sum + num);
-            const resta = promptNumbers.reduce((rest, num) => rest - num);
-            const multiplica = promptNumbers.reduce((mult, num) => mult * num);
-            const divide = promptNumbers.reduce((div, num) => div / num).toFixed(3);
+    const raizCuadrada = Math.sqrt(promptNumbers[0]);
 
-            calculos.push('\nAdd: ' + suma + '\nSubtract: ' + resta + '\nMultiply: ' + multiplica + '\nDivide: ' + divide);
-            return calculos;
-        } else {
-            const raizCuadrada = Math.sqrt(promptNumbers[0]);
+    calculos.push(raizCuadrada.toFixed(3).replace());
+    return calculos;
+  };
 
-            calculos.push(raizCuadrada.toFixed(3).replace());
-            return calculos;
+  resultados = calcular();
 
-        }
+  // Imprimir resultados
+  const imprimirResultados = () => {
+    if (promptNumbers.length > 2) {
+      alert(
+        "The entered numbers are " +
+          promptNumbers +
+          ", and the results are: " +
+          resultados
+      );
+    } else {
+      alert(
+        "The entered number is: " +
+          promptNumbers +
+          ", and its square root is: " +
+          resultados
+      );
     }
-    resultados = calcular();
+  };
 
-    //imprimir resultados
-    const imprimirResultados = () => {
-        if(promptNumbers.length > 2) {
-            alert('The entered numbers are ' + promptNumbers + ', and the results are: ' + resultados);
-        } else {
-            alert('The entered number is: ' + promptNumbers + ', and its square root is: ' + resultados);
-        }
+  imprimirResultados();
+
+  // Preguntar si recalcular
+
+  const reCalcular = () => {
+    if (confirm("Do you want to enter another number?")) {
+      return calculadoraPro();
     }
-    imprimirResultados();
 
-    //preguntar si recalcular
+    alert("Have a nice day!");
+  };
 
-    const reCalcular = () => {
-        if(confirm('Do you want to enter another number?')){
-            return calculadoraPro();
-        } else {
-            alert('Have a nice day!');
-        }
-    }
-    reCalcular();
+  reCalcular();
 }
 
 calculadoraPro();
